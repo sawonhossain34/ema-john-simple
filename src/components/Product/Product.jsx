cbff853f38ad409b6629590a000c565cdd282660
sawonhossain34 <sawonhossain3491@gmail.com>
2023-03-27 13:35:18 +0600

    product name price,rating,manufual and btn add hover set

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Product.css'
 const Product = (props) => {
-    const {img,name,price} = props.product;
+    const {img,name,price,seller,ratings} = props.product;
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <h6>{name}</h6>
+            <div className='product-info'>
+                <h6 className='product-name'>{name}</h6>
+                <p>Price : ${price}</p>
+                <p>Manufacturer :{seller}</p>
+                <p>Rating : {ratings} star </p>
+            </div>
+            <button className='btn-cart'>Add To Cart</button>
         </div>
     );
 };
