16875486cf96f04d27e1f6980693ce15db7171e8
sawonhossain34 <sawonhossain3491@gmail.com>
2023-03-27 11:02:06 +0600

    nav ber set

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import './Header.css';
+import logo from '../../images/Logo.svg'
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is header </h1>
-        </div>
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div>
+            <a href="/shop">shop</a>
+            <a href="/order">order</a>
+            <a href="/inventory">Inventory</a>
+            <a href="/login">Login</a>
+            </div>
+        </nav>
     );
 };
 
