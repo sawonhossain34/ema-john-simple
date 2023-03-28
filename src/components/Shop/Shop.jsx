b9823263c496cd03f462b156bcb3275f5a94fd64
sawonhossain34 <sawonhossain3491@gmail.com>
2023-03-28 12:14:15 +0600

    get id localstorage and countity

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -12,11 +13,17 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, [])
 
+    useEffect( () => {
+        const storedCart = getShoppingCart();
+        // console.log(storedCart);
+    } ,[])
+
     const handleAddToCart = (product) => {
         // console.log(product);
         console.log(cart);
         const newCart = [...cart, product];
         setcart(newCart)
+        addToDb(product.id);
     }
     return (
         <div className='shop-container'>
