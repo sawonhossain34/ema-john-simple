767037a4d32ec3652a1cc2377885339b6c947463
sawonhossain34 <sawonhossain3491@gmail.com>
2023-04-17 17:37:42 +0600

    set create and useContext data pass

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Header.css';
 import logo from '../../images/Logo.svg'
+import { AuthContext } from '../Providers/AuthProvider';
 
 const Header = () => {
+
+    const {user} = useContext(AuthContext);
+    console.log(user);
+
     return (
         <nav className='header'>
             <img src={logo} alt="" />
@@ -12,6 +17,7 @@ const Header = () => {
             <a href="/inventory">Inventory</a>
             <a href="/login">Login</a>
             <a href="/signup">SignUp</a>
+            {user && <span>Wellcome{user.email}</span>}
             </div>
         </nav>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import Login from './components/Login/Login';
 import cartProductsLoader from './loaders/cartproductsLoaders';
 import CheckOut from './components/CheckOut/CheckOut';
 import SignUp from './components/SignUp/SignUp';
+import AuthProvider from './components/Providers/AuthProvider';
 
 const router = createBrowserRouter([
   {
@@ -52,6 +53,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
- <RouterProvider router={router} />
+    <AuthProvider>
+    <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
