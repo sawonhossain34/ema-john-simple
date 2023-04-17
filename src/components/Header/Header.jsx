aa0e335b5a1b8a7860643e164dccec0ddf5a713c
sawonhossain34 <sawonhossain3491@gmail.com>
2023-04-17 20:11:58 +0600

    logOut and onAuthStateChange useEffect use

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,11 +5,13 @@ import { AuthContext } from '../Providers/AuthProvider';
 
 const Header = () => {
 
-    // const {user,logOut} = useContext(AuthContext);
+    const {user,logOut} = useContext(AuthContext);
 
-    // const handleLogOut= () => {
-
-    // }
+    const handleLogOut= () => {
+        logOut()
+        .then(result => { })
+        .catch(error => console.error(error));
+    }
     
 
     return (
@@ -21,7 +23,7 @@ const Header = () => {
             <a href="/inventory">Inventory</a>
             <a href="/login">Login</a>
             <a href="/signup">SignUp</a>
-            {/* {user && <span>Wellcome{user.email} <button onClick={handleLogOut}>Sign Out</button></span>} */}
+            {user && <span className='text-white'>Wellcome{user.email} <button onClick={handleLogOut}>Log Out</button></span>}
             </div>
         </nav>
     );
--- src/components/Providers/AuthProvider.jsx
+++ src/components/Providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import app from '../../firebase/firebase.config';
 
 
@@ -19,8 +19,19 @@ const AuthProvider = ({children}) => {
     }
 
     const logOut = () => {
-        signOut(auth);
+        return signOut(auth);
     }
+
+    // observer user auth state
+    useEffect( () => {
+      const unsubscribe =  onAuthStateChanged(auth , currentUser => {
+            setUser(currentUser);
+        })
+        // stop observing while unmounting
+        return () => {
+            return unsubscribe;
+        }
+    } ,[])
 
     const authInfo={
 user,createUser,signIn,logOut
